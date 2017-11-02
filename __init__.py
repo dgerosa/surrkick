2394331b3fceaeae2301afffd2ca4306fa1a3e6c
Davide Gerosa <dgerosa@caltech.edu>
2017-11-01 19:00:45 -0500

    structure as python module

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from surrkick import *
+__author__ = surrkick.__author__
+__email__ = surrkick.__email__
+__license__ = surrkick.__license__
+__version__ = surrkick.__version__
+__doc__ = surrkick.__doc__
