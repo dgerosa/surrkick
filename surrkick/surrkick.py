c9e28b8a57c00ebcdd4a8712af75a9247aee8fa3
Davide Gerosa <dgerosa@caltech.edu>
2018-01-30 14:29:40 -0800

    full path for NR aux files

--- surrkick/surrkick.py
+++ surrkick/surrkick.py
@@ -30,7 +30,7 @@ import json
 __author__ = "Davide Gerosa, Francois Hebert"
 __email__ = "dgerosa@caltech.edu"
 __license__ = "MIT"
-__version__ = "1.0"
+__version__ = "1.0.0"
 __doc__+="\n\n"+"Authors: "+__author__+"\n"+\
         "email: "+__email__+"\n"+\
         "Licence: "+__license__+"\n"+\
@@ -1493,8 +1493,8 @@ class plots(object):
         fig = plt.figure(figsize=(6,6))
         ax = fig.add_axes([0,0,1.25,0.6])
 
-        nr100 = np.loadtxt("nr_comparison_data/nr_kicks_t100.dat")
-        nr4500 = np.loadtxt("nr_comparison_data/nr_kicks_t4500.dat")
+        nr100 = np.loadtxt(os.path.dirname(__file__)+"/"+"nr_comparison_data/nr_kicks_t100.dat")
+        nr4500 = np.loadtxt(os.path.dirname(__file__)+"/"+"nr_comparison_data/nr_kicks_t4500.dat")
 
         def _nr_surr_comparison_data_helper(nr_data, t):
             kicks = []
@@ -1581,7 +1581,7 @@ class plots(object):
         axt = fig.add_axes([0, main_h + gap, main_w, hist_h])
         axr = fig.add_axes([main_w + gap, 0, hist_h, main_h])
 
-        nr4500 = np.loadtxt("nr_comparison_data/nr_kicks_t4500.dat")
+        nr4500 = np.loadtxt(os.path.dirname(__file__)+"/"+"nr_comparison_data/nr_kicks_t4500.dat")
 
         # duplicated from histogram plot
         def _nr_surr_comparison_data_helper(nr_data, t):
@@ -1676,7 +1676,7 @@ class plots(object):
         ax_ul = fig.add_axes([0, h + gap, w, h])
 
         # duplicated from histogram plot
-        basename = "nr_comparison_data/profile_case_id_"
+        basename = os.path.dirname(__file__)+"/"+"nr_comparison_data/profile_case_id_"
         cases = ["0021", "0283", "0353", "3144"]
         filename = "nr_comparison_profiles.pkl"
         if not os.path.isfile(filename):
